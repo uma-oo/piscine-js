29c9aa720fbe97141f2f77f0044eb3a351303d89
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-19 13:36:38 +0100

    add declarations.js

--- declarations.js
+++ declarations.js
@@ -1,7 +1,9 @@
-const escapStr = `Hello i\'m new to this Language 
+const escapeStr = `Hello i\'m new to this Language 
  "Please Go easy on me "
  "i'm just a little girl /:"
 `
+
+
 
 const arr = [4, '2']
 const obj = {
