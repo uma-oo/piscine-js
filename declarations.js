a152897c7c93d30c899743a02a92ec6e2737ec03
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-19 13:55:41 +0100

    ifares helped me out

--- declarations.js
+++ declarations.js
@@ -16,7 +16,7 @@ const obj = {
 };
 Object.freeze(obj);
 
-const nested = {
+const nested =Object.freeze({
     arr : [4, undefined, '2'],
     obj : {
         str : obj.str,
@@ -25,5 +25,5 @@ const nested = {
     
     }
     
-};
-Object.freeze(nested , nested.obj);
+});
+Object.freeze(nested.obj, nested.arr);
