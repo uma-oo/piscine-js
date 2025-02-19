f31f884d154348c81e3197bc96f36c2c5cca4bbe
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-19 15:11:10 +0100

    delete the freeze useless here

--- mutability.js
+++ mutability.js
@@ -1,4 +1,3 @@
-
 const clone1 = {
     name:person.name,
     age:person.age,
@@ -9,19 +8,7 @@ const clone2 = {
     age:person.age,
     country: person.country
 }
-Object.freeze(clone1)
-Object.freeze(clone2)
-
 const samePerson = person
-
-
 console.log(clone1===clone2)
-
 person.age+=1
 person.country='FR'
-
-
-
-
-
-
