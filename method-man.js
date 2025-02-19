504df39ed42865edcc4833e5df3f144f2346d633
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-19 17:27:48 +0100

    add some fixes

--- method-man.js
+++ method-man.js
@@ -3,5 +3,5 @@
 let words= (str) => str.split(' ')
 let sentence= (sliceOfstrings)=> sliceOfstrings.join(" ")
 let yell= (str)=> str.toUpperCase()
-let whisper= (str)=> str.toLowerCase()
+let whisper= (str)=> "*"+str.toLowerCase()+"*"
 let capitalize=(str)=> str[0].toUpperCase()+str.slice(1)
