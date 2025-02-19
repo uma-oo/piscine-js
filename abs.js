cf8e3b50f4ef8d8f9bedca8fe490d61f215fa30b
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-19 17:39:18 +0100

    add some unsure code

---
+++ abs.js
@@ -0,0 +1,14 @@
+
+function isPositive(number){
+    if( number>0) return true 
+    else return false
+
+}
+
+
+function abs(number){
+    if (number<0 )return number*-1
+    else return number
+}
+
+
