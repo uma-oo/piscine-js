4f8c0bdf26dceea54d1255b40fb49a1a31c665df
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-19 17:47:04 +0100

    add some code

---
+++ sign.js
@@ -0,0 +1,14 @@
+
+
+function sign(num){
+    if (num>0) return 1
+    else if (num<0) return -1
+    else return 0
+}
+
+
+function sameSign(num1, num2){
+    if (sign(num1)===sign(num2)) return true
+    else return false 
+
+}
