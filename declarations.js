b3a4e33c8bf6b4d125ca0a6958d3ce4ac4b7fe20
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-19 13:35:13 +0100

    add declarations.js

---
+++ declarations.js
@@ -0,0 +1,20 @@
+const escapStr = `Hello i\'m new to this Language 
+ "Please Go easy on me "
+ "i'm just a little girl /:"
+`
+
+const arr = [4, '2']
+const obj = {
+    str : "oumayma",
+    num : 25,
+    bool : true , 
+    undef : undefined
+}
+
+const nested = {
+    arr : [4, undefined, '2'],
+    str : "oumayma",
+    num : 25,
+    bool : true 
+}
+Object.freeze(nested, obj)
