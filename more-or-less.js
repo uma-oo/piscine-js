f5a99dff87fb2fd7eca0f27c23482ea8df849c9e
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-19 14:54:07 +0100

    add more-or-less.js

---
+++ more-or-less.js
@@ -0,0 +1,17 @@
+
+
+function more(number) {
+    return number + 1;
+}
+function less(number){
+    return number-1;
+
+}
+
+function add(a, b){
+    return a+b;
+}
+
+function sub(a, b){
+    return a-b;
+}
