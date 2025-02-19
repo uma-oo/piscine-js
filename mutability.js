256ac2497947c343e11a4597219de27a9561484c
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-19 14:39:44 +0100

    add some code

--- mutability.js
+++ mutability.js
@@ -1,13 +1,21 @@
+
 const clone1 = {
     name:person.name,
     age:person.age,
     country: person.country
 }
-const clone2 = clone1
+const clone2 = {
+    name:person.name,
+    age:person.age,
+    country: person.country
+}
 Object.freeze(clone1)
 Object.freeze(clone2)
 
 const samePerson = person
+
+
+console.log(clone1===clone2)
 
 person.age+=1
 person.country='FR'
