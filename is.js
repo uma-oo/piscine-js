773d6053fd8782263f6581d1088e67806898ee92
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-20 18:47:19 +0100

    ghir kanjrb

--- is.js
+++ is.js
@@ -1,15 +1,68 @@
 
-const is = {}
 
-function num(argument) {
+
+// is.num = (argument) => {
+//     return typeof (argument) === "number" ? true : false;
+// }
+
+function  num(argument){
     return typeof (argument) === "number" ? true : false;
 }
 
-function nan(argument){
-    return typeof(argument)=== "number" && argument===NaN ? true : false
+function nan(argument) {
+    return typeof (argument) === "number" && Number.isNaN(argument) ? true : false
 }
 
-console.log(num(5))
-console.log(nan(NaN))
-console.log(typeof(NaN))
-console.log(NaN)
+
+function str(argument) {
+    return typeof (argument) === "string" ? true : false
+}
+
+function bool(argument) {
+    return typeof (argument) === "boolean" ? true : false
+
+}
+
+function undef(argument) {
+    return typeof (argument) === "undefined" ? true : false
+}
+
+function def(argument) {
+    return typeof (argument) !== "undefined" ? true : false
+}
+
+
+
+function arr(argument) {
+    return Array.isArray(argument) ? true : false;
+}
+
+function obj(argument) {
+    return typeof (argument) === "object" ? true : false;
+}
+
+function fun(argument) {
+    return typeof (argument) === "function" ? true : false;
+}
+
+
+function truthy(argument) {
+    return argument !== "" && argument !== 0 && argument !== -0 && argument !== null && argument !== undefined && argument !== 0n && argument !== NaN && argument !== false ? true : false
+}
+
+function falsy(argument) {
+    return !truthy(argument)
+}
+
+
+is.num = num()
+is.nan = nan()
+is.str = str()
+is.bool= bool()
+is.falsy= falsy()
+is.truthy= truthy()
+is.fun = fun()
+is.obj = obj()
+is.arr=arr()
+is.undef= undef()
+is.def=def()
---
+++ test.js
@@ -0,0 +1,10 @@
+let x  = "100" / "10";
+let y  = "100" * "10";
+let z = "100" - "10";
+let w= "100"+"10"
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(w)
+
+console.log(typeof  [1,2])
