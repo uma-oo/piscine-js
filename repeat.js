7f645c34845aeb1f72f1559ddda392878f4729dc
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-22 12:10:33 +0100

    add fixes

--- repeat.js
+++ repeat.js
@@ -2,9 +2,11 @@
 
 function repeat(str, nbr) {
     let new_str=""
-    for (let i = 1; i < nbr; i++) {
+    for (let i = 1; i <= nbr; i++) {
         new_str += str
     }
     return new_str
 }
+
+
 
