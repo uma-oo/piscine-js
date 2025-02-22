3b910aab393e19786f27767ba167ceaf68f1c001
ooumayma <oumayma.elfahsi99@gmail.com>
2025-02-22 12:33:38 +0100

    add code bida2i

---
+++ elementary.js
@@ -0,0 +1,36 @@
+
+
+
+
+function multiply(a, b) {
+    result = 0
+    for (let i = 1; i <= b; i++) {
+        result += a
+
+    }
+    return result
+
+}
+
+// integer division operator // hada f python ( z3ma division euclidienne ;)
+
+function divide(a, b) {
+    result=0
+   
+    while(a-b>=0){
+      result+=1
+      a=a-b
+    }
+   return result
+}
+
+function modulo(a, b) {
+    return a-multiply(b, divide(a,b))
+
+}
+
+
+console.log(multiply(5,3) )
+
+console.log(divide(15,5) )
+console.log(modulo(9,5) )
