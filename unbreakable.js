eb8ba0a12c9008e686a7ae29ec429f0f36f36154
Oumayma El-fahsi <ooumayma>
2025-02-23 13:22:19 +0000

    add some code

--- unbreakable.js
+++ unbreakable.js
@@ -1,22 +1,36 @@
 
-function split(str, sep="") {
+function split(str, sep = "") {
     let new_array = []
-    let words=""
-    for (let i=0; i<str.length;i++){
-        console.log(str.slice(i, i+sep.length))
-        if(str.slice(i, i+sep.length) !==sep ){
-           words=words+str.slice(i, i+sep.length)
-           console.log("words", words)
-           i=i+sep.length-1
+    if (sep === "" ) {
+        for (let char of str){
+            new_array.push(char)
+        }
+        return new_array
+    }
+    let words = ""
+    for (let i = 0; i < str.length; i++) {
+        if (str.slice(i, i + sep.length) !== sep) {
+            words = words + str[i]
+            console.log(words)
+        } else if (i === str.length - sep.length && str.slice(i, i + sep.length) === sep) {
+            new_array.push(words)
+            words = ""
+            i = str.length
+            new_array.push("")
         } else {
             new_array.push(words)
-            words=""
-            i=i+sep.length-1
+            words = ""
+            i = i + sep.length - 1
         }
     }
-   if (words!=="") {
-      new_array.push(words)
-   }
+
+    if (words !== "") {
+        new_array.push(words)
+    }
+    
+    if( new_array.length===0){
+        new_array.push("")
+    }
 
     return new_array
 
@@ -27,16 +41,12 @@ function join(array, sep = " ") {
     let new_str = ""
     for (let i = 0; i < array.length; i++) {
         if (i !== array.length - 1) {
-            new_str += array[i] + sep 
-        }else {
-            new_str+= array[i]
+            new_str += array[i] + sep
+        } else {
+            new_str += array[i]
         }
 
     }
     return new_str
 }
 
-
-
-console.log(split('ee,ff,g,', ','))
-console.log(split('rrirr', 'rr'))
