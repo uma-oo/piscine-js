28b9fe76683eb5848d97425f4e2ae162407bd6b1
Oumayma El-fahsi <ooumayma>
2025-02-24 13:29:14 +0000

    add some code

---
+++ greedy-url.js
@@ -0,0 +1,16 @@
+
+
+function getURL(string){
+    let re = /((http:\/\/|https:\/\/)[a-zA-Z0-9]+.{1}[a-zA-Z0-9]+)/g
+    let words = string.match(re)
+    return words
+}
+
+
+function greedyQuery(string){
+
+}
+
+function notSoGreedy(string){
+
+}
