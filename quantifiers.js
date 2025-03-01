7068b37335345dc20eeb862b646ffb521ce0ecc2
ooumayma <oumayma.elfahsi99@gmail.com>
2025-03-01 18:12:01 +0000

    add some code

--- quantifiers.js
+++ quantifiers.js
@@ -23,5 +23,10 @@ function some(array_data, func) {
 
 
 function none(array_data, func) {
-    return !every(array_data, func)
+    for (let i = 0; i < array_data.length; i++) {
+        if (func(array_data[i], i, array_data)) {
+            return false
+        }
+    }
+    return true
 }
