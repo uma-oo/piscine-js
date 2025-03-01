4b0ccb973f42b028186b2434a62564a9fc6be422
ooumayma <oumayma.elfahsi99@gmail.com>
2025-03-01 16:29:46 +0000

    add some edits

--- mapper.js
+++ mapper.js
@@ -15,8 +15,36 @@ function flatMap(array_data, func) {
     for (let i = 0; i < array_data.length; i++) {
         new_array.push(func(array_data[i], i, array_data))
     }
-    return new_array.flat()
+    return flat(new_array)
 }
 
 
+
+function flat(array_data, depth = 1) {
+    let new_array = []
+    while (depth >=1) {
+        for ( let i=0 ; i<array_data.length; i++){
+            if (Array.isArray(array_data[i])) {
+                new_array.push(...array_data[i])
+            } else {
+                new_array.push(array_data[i])
+            }
+            depth--
+        }
+      
+    }
+   return new_array
+}
+
+
+
+
+
+
+
+
+
+
+
+console.log(flatMap([1, 2, 3], (n) => [n, n]));
 
