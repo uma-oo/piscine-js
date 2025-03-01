3d06d9952aa5a717fc343ec7e64bc56e5b390dea
ooumayma <oumayma.elfahsi99@gmail.com>
2025-03-01 17:35:37 +0000

    add some edits

--- reduce.js
+++ reduce.js
@@ -38,8 +38,3 @@ function reduceRight(array_data, func, accumulator=0) {
 }
 
 
-const adder = (a, b) => a + b
-console.log(fold([1, 2, 3], adder, 2)) // returns 8 (2 + 1 + 2 + 3)
-console.log(foldRight([1, 2, 3], adder, 2)) // returns 8 (2 + 3 + 2 + 1)
-console.log(reduce([1, 2, 3], adder)) // returns 6 (1 + 2 + 3)
-console.log(reduceRight([1, 2, 3], adder)) // returns 6 (3 + 2 + 1)
