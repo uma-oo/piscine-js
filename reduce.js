b57c1257f3656bca5b7a89fed353a0cbfbfd02bc
ooumayma <oumayma.elfahsi99@gmail.com>
2025-03-01 18:05:12 +0000

    add some fixes

--- reduce.js
+++ reduce.js
@@ -1,6 +1,6 @@
 
 
-function fold(array_data, func, accumulator=array_data[0]) {
+function fold(array_data, func, accumulator) {
     for (let i = 0; i < array_data.length; i++) {
         accumulator = func(accumulator, array_data[i], i, array_data)
     }
@@ -9,7 +9,7 @@ function fold(array_data, func, accumulator=array_data[0]) {
 
 
 
-function foldRight(array_data, func,  accumulator=array_data[array_data.length-1]) {
+function foldRight(array_data, func, accumulator) {
     for (let i = array_data.length - 1; i >= 0; i--) {
         accumulator = func(accumulator, array_data[i], i, array_data)
     }
@@ -18,24 +18,34 @@ function foldRight(array_data, func,  accumulator=array_data[array_data.length-1
 }
 
 
-function reduce(array_data, func, accumulator=array_data[0]) {
+function reduce(array_data, func, accumulator) {
     if (array_data.length < 1) {
         throw new Error("")
     } else {
+        if (!accumulator) {
+            accumulator = array_data[0]
+        }
         let res = fold(array_data.slice(1), func, accumulator)
         return res
     }
 
 }
 
-function reduceRight(array_data, func, accumulator=array_data[array_data.length-1]) {
+function reduceRight(array_data, func, accumulator) {
     if (array_data.length < 1) {
         throw new Error("")
     } else {
-        let res = foldRight(array_data.slice(1), func, accumulator)
+        if (!accumulator) {
+            accumulator = array_data[array_data.length-1]
+        }
+        let res = foldRight(array_data.slice(0,array_data.length-1), func, accumulator)
         return res
     }
 }
+
+
+
+
 
 
 
