a538b71e74188810d997a89cda2a59c2b83dc509
ooumayma <oumayma.elfahsi99@gmail.com>
2025-03-03 15:55:34 +0000

    add some fixes

--- exo6/keycodes-symphony.js
+++ exo6/keycodes-symphony.js
@@ -3,7 +3,7 @@
 
 export function compose() {
     let colors = {
-        "a": "#FF0000 ",
+        "a": "#FF0000",
         "b": "#0000FF",
         "c": "#00FFFF",
         "d": "#006400 ",
@@ -14,7 +14,7 @@ export function compose() {
         "i": "#4B0082",
         "j": "#00A86B",
         "k": "#F0E68C",
-        "l": "#E6E6FA ",
+        "l": "#E6E6FA",
         "m": "#FF00FF",
         "n": "#000080",
         "o": "#FFA500",
@@ -23,43 +23,43 @@ export function compose() {
         "r": "#FF4500",
         "s": "#C0C0C0",
         "t": "#008080",
-         "u" :"#8B4513" ,
-         "v":"#EE82EE",
-         "w": "#F5DEB3" ,
-         "x" : "#F6EB61" ,
-         "y" :"#9ACD32",
-         "z" : "#7C7C7C" ,
-         "Space": "#FFFFFF" ,
+        "u": "#8B4513",
+        "v": "#EE82EE",
+        "w": "#F5DEB3",
+        "x": "#F6EB61",
+        "y": "#9ACD32",
+        "z": "#7C7C7C",
+        " ": "#FFFFFF",
     }
 
 
 
 
-document.addEventListener("keydown", (event) => {
+    document.addEventListener("keydown", (event) => {
 
-    let new_div
-    switch (event.key) {
-        case "Escape":
-            let all_divs = document.querySelectorAll("div")
-            for (let div of all_divs) {
-                div.remove()
-            }
-        case "Backspace":
-            if (document.body.lastChild) {
-                document.body.lastChild.remove()
-            }
-            break;
-        default:
-            new_div = document.createElement("div")
-            new_div.className = "note"
-            new_div.innerText = event.key
-            let key_value = event.key
-            new_div.style.backgroundColor= colors[key_value]
-            document.body.append(new_div)
-            break;
-    }
+        let new_div
+        switch (event.key) {
+            case "Escape":
+                let all_divs = document.querySelectorAll("div")
+                for (let div of all_divs) {
+                    div.remove()
+                }
+            case "Backspace":
+                if (document.body.lastChild) {
+                    document.body.lastChild.remove()
+                }
+                break;
+            default:
+                new_div = document.createElement("div")
+                new_div.className = "note"
+                new_div.innerText = event.key
+                let key_value = event.key
+                new_div.style.backgroundColor = colors[key_value]
+                document.body.append(new_div)
+                break;
+        }
 
-});
+    });
 
 
 }
