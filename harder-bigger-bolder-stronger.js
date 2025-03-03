26d8976d4bd27d272b26b17a06a42e2d20fba634
ooumayma <oumayma.elfahsi99@gmail.com>
2025-03-03 11:31:55 +0000

    add some code

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -9,15 +9,13 @@ const random = (min, max) => {
   }
 
 
-
 export function generateLetters(){
     const  alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
     for (let i =1; i<=120; i++){
         let index = random(0, alphabet.length-1)
-        console.log("index", index);
         let div = document.createElement("div")
         div.innerText = alphabet[index]
-        div.style.fontSize="130px"
+        div.style.fontSize=`${i+11}px`
         if (i<=40){
             div.style.fontWeight=300
         } else if (i<=80){
