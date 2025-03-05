4dafe55ac5f637e93bb90caf0f41af060a2290b0
ooumayma <oumayma.elfahsi99@gmail.com>
2025-03-05 16:24:53 +0000

    add some code

--- pick-omit.js
+++ pick-omit.js
@@ -5,10 +5,12 @@
 function pick(object, arr_or_arr_str) {
     let new_object = {}
     for (let [key, value] of Object.entries(object)) {
-        if (arr_or_arr_str.includes(key)) {
-            new_object[key] = value
-
+        for (let ele of arr_or_arr_str) {
+            if (ele === key) {
+                new_object[key] = value
+            }
         }
+
     }
     return new_object
 
@@ -19,10 +21,12 @@ function pick(object, arr_or_arr_str) {
 function omit(object, arr_or_arr_str) {
     let new_object = {}
     for (let [key, value] of Object.entries(object)) {
-        if (!arr_or_arr_str.includes(key)) {
-            new_object[key] = value
-
+        for (let ele of arr_or_arr_str) {
+            if (ele !== key) {
+                new_object[key] = value
+            }
         }
+
     }
     return new_object
 }
