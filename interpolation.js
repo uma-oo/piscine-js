44a5c2370630d555457a2deaa352b4962b341440
Oumayma El-fahsi <ooumayma>
2025-03-09 21:40:07 +0000

    add some code

--- interpolation.js
+++ interpolation.js
@@ -6,9 +6,9 @@ function interpolation(object) {
     let v = (object["end"] - object["start"]) / object["step"]
     let i = 0
     let x= start
+    console.log("v", v);
     while (i < object["step"]) {
-        console.log("x",x);
-        console.log("y",findEquation(object, x,v)); 
+        console.log(x, findEquation(object, x,v));
         setTimeout(() => {
             object.callback([x, findEquation(object, x,v)])
         }, v)
@@ -24,7 +24,7 @@ function interpolation(object) {
 // interpolation in maths 
 function findEquation(object, x_to_find, v) {
     const start = object["start"]
-    const f_start = (object["end"] - object["start"]) / object["step"]
+    const f_start =   object["duration"]/ object["step"]
     const end = start +((object["step"]-1)*v)
     const f_end = object["duration"]
     let f_x_to_find = (((f_end - f_start) / (end - start)) * (x_to_find - start)) + f_start
@@ -34,5 +34,5 @@ function findEquation(object, x_to_find, v) {
 
 
 let callback = ([x, y]) => console.log(x, y)
-let obj = { step: 10, start: 2, end: 6, duration: 4 , callback};
+let obj = { step: 5, start: 6, end: 2, duration: 6, waitTime: 10 , callback}
 console.log("hadi", interpolation(obj));
