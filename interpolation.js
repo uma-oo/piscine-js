898aec9f948c07390c5110ddd7b5c5a33879ab81
Oumayma El-fahsi <ooumayma>
2025-03-10 12:51:13 +0000

    add some code

--- interpolation.js
+++ interpolation.js
@@ -3,36 +3,24 @@
 
 function interpolation(object) {
     let start = object["start"]
-    let v = (object["end"] - object["start"]) / object["step"]
-    let i = 0
-    let x= start
-    console.log("v", v);
-    while (i < object["step"]) {
-        console.log(x, findEquation(object, x,v));
+    let gap_x = (object["end"] - object["start"]) / object["step"]
+    let gap_y = object["duration"] / object["step"]
+    console.log(start);
+    console.log(gap_x);
+    console.log(gap_y);
+    for (let i = 1; i <= object["step"]; i++) {
         setTimeout(() => {
-            object.callback([x, findEquation(object, x,v)])
-        }, v)
-        i++
-        x=Math.round((x+v)*100)/100
+            object.callback([(Math.round(start*100)/100), Math.round(gap_y * i*100)/100])
+            start += gap_x
+        }, gap_y * i);
     }
 
 
-
 }
 
 
-// interpolation in maths 
-function findEquation(object, x_to_find, v) {
-    const start = object["start"]
-    const f_start =   object["duration"]/ object["step"]
-    const end = start +((object["step"]-1)*v)
-    const f_end = object["duration"]
-    let f_x_to_find = (((f_end - f_start) / (end - start)) * (x_to_find - start)) + f_start
-    return Math.round(f_x_to_find * 100) / 100
-
-}
 
 
 let callback = ([x, y]) => console.log(x, y)
-let obj = { step: 5, start: 6, end: 2, duration: 6, waitTime: 10 , callback}
+let obj = { step: 5, start: 6, end: 2, duration: 6, waitTime: 10, callback }
 console.log("hadi", interpolation(obj));
