c4ad36de6c1b9511908ae67b51074641317f63d7
Your Name <you@example.com>
2025-03-13 12:15:43 +0000

    add some code

--- verydisco.mjs
+++ verydisco.mjs
@@ -5,14 +5,19 @@
 let arg = process.argv[2]
 let words = arg.split(" ")
 let new_sentence = ''
+
 for (let word of words) {
     let median = Math.ceil((word.length) / 2)
     let [first, ...rest] = word.split(word[median])
     rest = rest.join(word[median])
+    let left =word[median]
+    if (rest===undefined) rest=""
+    if (first===undefined) first=""
+    if( word[median]===undefined) left=""
     if (rest.length < first.length) {
-        new_sentence += `${word[median]}${rest}${first} `
+        new_sentence += `${left}${rest}${first} `
     } else {
-        new_sentence += `${rest}${first}${word[median]} `
+        new_sentence += `${rest}${first}${left} `
     }
 
 }
