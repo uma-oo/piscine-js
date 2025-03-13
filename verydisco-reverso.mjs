77d8666ae296bc8c12469ed434a19a4f54601660
ooumayma <oumayma.elfahsi99@gmail.com>
2025-03-13 13:30:39 +0000

    add some code

---
+++ verydisco-reverso.mjs
@@ -0,0 +1,27 @@
+
+
+import { readFile } from 'node:fs/promises';
+let filename = process.argv[2]
+
+function convert(sentence) {
+
+    let words = sentence.split(" ")
+    let new_sentence = ''
+
+    for (let i in words) {
+        let word = words[i]
+        let median = Math.ceil((word.length) / 2)
+        new_sentence += word.slice(median) + word.slice(0, median)
+        if (i != words.length - 1) new_sentence += ` `
+    }
+    return new_sentence
+}
+
+
+const data_read = await readFile(filename, "utf-8", (err, data) => {
+    if (err) return err
+    return convert(data)
+
+})
+
+console.log(data_read);
