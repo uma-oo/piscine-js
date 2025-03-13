3326fed5d74e2b7fd4ead4d84ca3cc891edfc374
Your Name <you@example.com>
2025-03-13 16:22:18 +0000

    add some code

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -4,14 +4,22 @@
 function retry(count, callback) {
     return async function (...args) {
         for (let i = 0; i <= count; i++) {
+            console.log(i);
             let res = callback(...args).then(
-                (result) => { console.log("result",result);   return  result}
+                (result) => { console.log(i, result); return result }
+            ).catch(
+                (err) => { console.log("err", err); return err}
             )
-            if (res){ return res}
-            else if (!res && count===i){
-                console.log("hnaaaa");
-               throw res
+            if (res && String(res).startsWith("Error")) return res
+            else if (count === i && res) {
+                return res
             }
+
+            // if (res){ return res}
+            // else if (!res && count===i){
+            //     console.log("hnaaaa");
+            //    throw res
+            // }
         }
     }
 }
