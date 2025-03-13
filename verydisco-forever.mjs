6539f53fb39ad6814ff40e3c46997e45663440ea
ooumayma <oumayma.elfahsi99@gmail.com>
2025-03-13 13:17:47 +0000

    add some code

--- verydisco-forever.mjs
+++ verydisco-forever.mjs
@@ -1,6 +1,7 @@
 
 import { writeFile } from 'node:fs/promises';
 
+
 let arg = process.argv[2]
 
 function convert(sentence) {
@@ -11,26 +12,20 @@ function convert(sentence) {
     for (let i in words) {
         let word = words[i]
         let median = Math.ceil((word.length) / 2)
-        let [first, ...rest] = word.split(word[median])
-        rest = rest.join(word[median])
-        let left = word[median]
-        if (rest === undefined) rest = ""
-        if (first === undefined) first = ""
-        if (word[median] === undefined) left = ""
-        if (rest.length < first.length) {
-            new_sentence += `${left}${rest}${first}`
-        } else {
-            new_sentence += `${rest}${first}${left}`
-        }
-        // console.log(typeof i , typeof(words.length-1), i);  in kaykun index string ghariiib 
-        if (i!=words.length-1) new_sentence+=` ` , console.log("hna");
+
+        new_sentence += word.slice(median) + word.slice(0, median)
+
+        if (i != words.length - 1) new_sentence += ` `
     }
 
     return new_sentence
 }
 
-writeFile("verydisco-forever.txt", convert(arg), (err=> {
-    if (err) throw err 
+console.log(convert(arg));
+
+
+writeFile("verydisco-forever.txt", convert(arg), (err => {
+    if (err) throw err
 }))
 
 // test mrra yduuz 
