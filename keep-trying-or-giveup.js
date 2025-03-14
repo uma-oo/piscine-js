b5330ab13e4b26ca3971961db812d8bcb0eb9e0a
Oumayma <oumaymaelfahsi@gmail.com>
2025-03-14 00:55:04 +0000

    add some code

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,72 +1,41 @@
-
-// const callback = await callback
-
 function retry(count, callback) {
-    return async function (...args) {
-        for (let i = 0; i <= count; i++) {
-            console.log(i);
-            let res = callback(...args).then(
-                (result) => { console.log(i, result); return result }
-            ).catch(
-                (err) => { console.log("err", err); return err}
-            )
-
-            // 
-            console.log("hereeeee");
-            console.log(res);
-            if (res && String(res).startsWith("Error")) return res // omg rah l3kss li khassu ykuun
-            else if (count === i && res) {
-                return res
-            }
-            // if (res){ return res}
-            // else if (!res && count===i){
-            //     console.log("hnaaaa");
-            //    throw res
-            // }
-        }
-    }
+  return async function (...args) {
+      for (let i = 0; i <= count; i++) {
+          let res = await  callback(...args).then(
+              (result) => { return result; }
+          ).catch(
+              (err) => { return err }
+          );
+          if (!(res instanceof Error)) {
+            return res;
+          }
+          else if (count === i && ( res instanceof Error)) {
+              throw new Error(res.message)
+          }
+      }
+  }
 }
 
 
 
+function timeout(delay, callback) {
+  return async function (...args) {
+    const before = Date.now()
+    let res = await callback(...args).then(
+      (result) => {
+        const now = Date.now()
+        return [now, result]
+      }).catch(err => err)
 
-const t = (f) => tests.push(f)
+    if (res.length === 2) {
+      let [now, result] = res
+      if (now - before >= delay) { // tests khdamin setTO so no need /1000
+        throw new Error("timeout")
+      }
+      return result
+    }
 
-const fail = (q) =>
-  q.then(
-    (v) => Promise.reject('should fail'),
-    (e) => e.message,
-  )
-
-t(async ({ eq, ctx }) => eq(await retry(0, ctx.failNTimes(0))(ctx.r), [ctx.r]))
-t(async ({ eq, ctx }) => eq(await retry(3, ctx.failNTimes(3))(ctx.r), [ctx.r]))
-t(async ({ eq, ctx }) =>
-  eq(await retry(10, ctx.failNTimes(5))(ctx.r, ctx.r), [ctx.r, ctx.r]),
-)
-t(async ({ eq, ctx }) =>
-  eq(await fail(retry(3, ctx.failNTimes(9))(ctx.r)), `x:${ctx.r}`),
-)
-
-t(async ({ eq, ctx }) => eq(await timeout(2, ctx.delayed(0))(ctx.r), [ctx.r]))
-t(async ({ eq, ctx }) =>
-  eq(await timeout(2, ctx.delayed(0))(ctx.r, ctx.r), [ctx.r, ctx.r]),
-)
-t(async ({ eq, ctx }) =>
-  eq(await fail(timeout(2, ctx.delayed(4))(ctx.r)), 'timeout'),
-)
-
-Object.freeze(tests)
-
-export const setup = () => ({
-  r: Math.random().toString(36).slice(2),
-  failNTimes: (n) => async (...v) =>
-    --n < 0 ? v : Promise.reject(Error(`x:${v}`)),
-  delayed: (delay) => (...v) => new Promise((s) => setTimeout(s, delay, v)),
-})
-
-
-
-
-
-
+   throw new Error("timeout")
+  }
+}
 
